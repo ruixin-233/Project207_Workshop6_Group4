e6ec558f53477076c0632347cf1b568d659e1b3b
Ruixin Luo <luo.rxin@gmail.com>
2021-10-21 20:38:12 -0600

    Customer Table

--- src/main/java/com/example/project207_workshop6_group4/CustomerController.java
+++ src/main/java/com/example/project207_workshop6_group4/CustomerController.java
@@ -1,5 +1,93 @@
+/**
+ * Sample Skeleton for 'customer-list.fxml' Controller Class
+ */
+
 package com.example.project207_workshop6_group4;
+
+import java.net.ConnectException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class CustomerController {
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="tvCustomer"
+    private TableView<Customer> tvCustomer; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustomerId"
+    private TableColumn<Customer, Integer> colCustomerId; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustFirstName"
+    private TableColumn<Customer, String> colCustFirstName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustLastName"
+    private TableColumn<Customer, String> colCustLastName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustAddress"
+    private TableColumn<Customer, String> colCustAddress; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustCity"
+    private TableColumn<Customer, String> colCustCity; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustProv"
+    private TableColumn<Customer, String> colCustProv; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustPostal"
+    private TableColumn<Customer, String> colCustPostal; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustCountry"
+    private TableColumn<Customer, String> colCustCountry; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustBusPhone"
+    private TableColumn<Customer, String> colCustBusPhone; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colCustEmail"
+    private TableColumn<Customer, String> colCustEmail; // Value injected by FXMLLoader
+
+    @FXML // fx:id="colAgentId"
+    private TableColumn<Customer, Integer> colAgentId; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert colCustomerId != null : "fx:id=\"colCustomerId\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustFirstName != null : "fx:id=\"colCustFirstName\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustLastName != null : "fx:id=\"colCustLastName\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustAddress != null : "fx:id=\"colCustAddress\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustCity != null : "fx:id=\"colCustCity\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustProv != null : "fx:id=\"colCustProv\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustPostal != null : "fx:id=\"colCustPostal\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustCountry != null : "fx:id=\"colCustCountry\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustBusPhone != null : "fx:id=\"colCustBusPhone\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colCustEmail != null : "fx:id=\"colCustEmail\" was not injected: check your FXML file 'customer-list.fxml'.";
+        assert colAgentId != null : "fx:id=\"colAgentId\" was not injected: check your FXML file 'customer-list.fxml'.";
+
+        getCustomers();
+
+        tvCustomer.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                onOpenDialog(tvCustomer.getSelectionModel().getSelectedIndex());
+            }
+        });
+    }
+
+    private void getCustomers() {
+        ConnectException conn = DriverManager.getConnection();
+    }
+
+    private void onOpenDialog(int selectedIndex) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(""));
+    }
 }
